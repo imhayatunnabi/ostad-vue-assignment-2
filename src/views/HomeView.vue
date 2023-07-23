9d82db416213c8301258d07b803e77aa2cd3760d
imhayatunnabi <nabilahmednafiahmed753@gmail.com>
2023-07-23 21:24:29 +0600

    update to final

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,43 @@
+<template>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div v-for="(image, index) in images" :key="index" :class="['carousel-item', index === 0 ? 'active' : '']">
+        <img :src="image" class="d-block w-100" alt="Slide {{ index + 1 }}">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, computed } from 'vue';
+
+const images = ref([
+  require('@/assets/image1.jpg'),
+  require('@/assets/image2.jpg'),
+  require('@/assets/image3.jpg'),
+]);
+
+const currentIndex = ref(0);
+
+const currentImage = computed(() => images.value[currentIndex.value]);
+
+const nextImage = () => {
+  currentIndex.value = (currentIndex.value + 1) % images.value.length;
+};
+
+const prevImage = () => {
+  currentIndex.value = (currentIndex.value - 1 + images.value.length) % images.value.length;
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+/* Add your custom styles here */
+</style>
